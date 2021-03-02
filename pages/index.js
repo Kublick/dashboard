9fec08d17a755f3d48efdde2cf84e635e9f1a917
Max Blancarte <max.blancarte@digitalvaultstudio.com>
2021-03-02 10:24:37 -0800

    added gridjs lib

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import TableExample from "../components/TableExample";
 
 export default function Home() {
 	return (
 		<>
-			<h1>Test</h1>
+			<TableExample />
 		</>
 	);
 }
